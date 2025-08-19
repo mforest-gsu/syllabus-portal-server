0096e5d261c548f7dfecc945a4d941ff7e1e2b42
Melody Kimball <mforest@gsu.edu>
2025-08-19 09:22:03 -0400

    Changed Core IMPACTS query to read D2L tables directly

--- src/Repository/SQL/GSU_SYLLABUS_VERIFICATION.sql
+++ src/Repository/SQL/GSU_SYLLABUS_VERIFICATION.sql
@@ -1,7 +1,14 @@
-select
-  TermCode AS "termCode",
-  CRN AS "crn"
+select distinct
+  CourseOffering.SisTermCode as "termCode",
+  CourseOffering.SisCrn as "crn"
 from
-  GSU_SYLLABUS_VERIFICATION
+  d2l_organizational_unit_ancestor OrgUnitAncestor,
+  d2l_organizational_unit CourseOffering
 where
-  TermCode = :termCode
+  OrgUnitAncestor.AncestorOrgUnitType = 'College' and
+  OrgUnitAncestor.AncestorOrgUnitCode = 'COL.090.CORE' and
+  OrgUnitAncestor.OrgUnitType = 'Section' AND
+  OrgUnitAncestor.OrgUnitCode like '%ECON1101%' AND
+  CourseOffering.OrgUnitId = OrgUnitAncestor.OrgUnitId AND
+  CourseOffering.SisTermCode = :TermCode AND
+  CourseOffering.SisCrn is not null
