a00e3f928552b60296162f30d06b2bb1637eb7a0
Melody Kimball <mforest@gsu.edu>
2025-09-02 10:45:51 -0400

    Added exception list to fall 2025 query

--- src/Repository/SQL/GSU_SYLLABUS_VERIFICATION.sql
+++ src/Repository/SQL/GSU_SYLLABUS_VERIFICATION.sql
@@ -10,4 +10,12 @@ where
   OrgUnitAncestor.OrgUnitType = 'Section' AND
   CourseOffering.OrgUnitId = OrgUnitAncestor.OrgUnitId AND
   CourseOffering.SisTermCode = :termCode AND
-  CourseOffering.SisCrn is not null
+  CourseOffering.SisCrn is not null and
+  -- exception list for Fall 2025
+  CourseSection.SisCrn not in (
+     '93492'  -- CONDUCTING-GRADUATE III|APCD 8003|Graduate course, remove from list
+    ,'93524'  -- T RELIGION & AGING|GERO 8700|Graduate course, remove from list
+    ,'93482'  -- PROJECT LAB 1|PRLB 8000|Graduate course, remove from list
+    ,'92872'  -- INDEPENDENT STUDY|WLC 6990|Graduate course, remove from list
+    ,'84135'  -- DISSERTATION|EDCI 9990|Dissertation courses don't have syllabi, remove from list
+  )
